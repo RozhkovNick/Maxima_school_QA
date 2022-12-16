0be5396f8b5b43a71714654d3f2e4124e25e79bf
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2022-12-16 20:36:24 +0300

    Update parameters.java

--- src/parameters.java
+++ src/parameters.java
@@ -1,7 +1,16 @@
-public class parameters {
-    public String name, lastname;
-    public int age;
-    public String hobby;
-    public float weight, height;
-    public String education;
+public class Parameters extends Main {
+    public Parameters(String name, String lastname, int age, String hobby, float weight, float height, String education){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.hobby = hobby;
+        this.weight = weight;
+        this.height =height;
+        this.education = education;
+    }
+    protected String name, lastname;
+    protected int age;
+    protected String hobby;
+    protected float weight, height;
+    protected String education;
 }
