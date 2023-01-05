7e8d7a396acb7fed4cde6dc96192efa63dd94ec7
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-01-05 17:38:08 +0300

    Delete Trasport directory

--- Trasport/src/Bmw.java
+++
@@ -1,8 +0,0 @@
-public class Bmw extends Parketnik{
-    @Override
-    protected void changeGear() {
-        super.changeGear();
-        // ADD 2 lines of code
-    }
-
-}
--- Trasport/src/Drive.java
+++
@@ -1,4 +0,0 @@
-public interface Drive {
-    void driveOn();
-    void driveOf();
-}
