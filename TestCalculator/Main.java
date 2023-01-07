ac79de11cd1a6021bbc7438f9d9dca618939f2f8
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-01-07 15:05:46 +0300

    Add files via upload

---
+++ TestCalculator/Main.java
@@ -0,0 +1,9 @@
+
+    public class Main {
+        public static void main (String[] args){
+            Calculator calculator = new Calculator();
+            calculator.getResult();
+//            System.out.println(calculator.getResult());
+            System.out.println(2/0);
+        }
+    }
