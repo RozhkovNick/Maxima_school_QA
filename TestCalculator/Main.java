f6d1978e374ace1f25c7f66386098c88923a3bc2
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-01-07 15:09:40 +0300

    Update Main.java

--- TestCalculator/Main.java
+++ TestCalculator/Main.java
@@ -3,7 +3,6 @@
         public static void main (String[] args){
             Calculator calculator = new Calculator();
             calculator.getResult();
-//            System.out.println(calculator.getResult());
-            System.out.println(2/0);
+            System.out.println(calculator.getResult());
         }
     }
