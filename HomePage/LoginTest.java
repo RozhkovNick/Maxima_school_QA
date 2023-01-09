089363f6315b61ce13ae2cffe352e129341bddd4
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-01-09 19:02:57 +0300

    Update LoginTest.java

--- HomePage/LoginTest.java
+++ HomePage/LoginTest.java
@@ -5,13 +5,10 @@ public class LoginTest extends BaseTest {
 
     public void loginTest() {
         File file = new File("C://Users//User//Desktop//LoginPassword.txt");
-
         LoginData ld = new LoginData("some login", "some password");
         LoginPage loginPage = new LoginPage();
         loginPage.login(ld);
-
         HomePage homePage = new HomePage();
         Assert.assertTrue(homePage.isImportantLabelAvaliable());
-
     }
 }
