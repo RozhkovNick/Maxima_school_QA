7945b431ec9a809c4a22657c6548200ed08a9d9a
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-01-16 20:34:18 +0300

    Delete CalculatorExpected directory

--- CalculatorExpected/Main.java
+++
@@ -1,8 +0,0 @@
-
-    public class Main {
-        public static void main (String[] args){
-            Calculator calculator = new Calculator();
-            calculator.getResult();
-            System.out.println(calculator.getResult());
-        }
-    }
