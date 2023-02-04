29168b2128af9d85126e294fcace41b4a385f6fe
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-02-04 14:16:40 +0300

    Add files via upload

--- TestNg/src/main/java/Main.java
+++ TestNg/src/main/java/Main.java
@@ -1,19 +1,20 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.List;
-
-public class Main {
+public class Main{
     @Test
-    public void main(){
-      WebDriver driver = new ChromeDriver();
+    public void main() {
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1980, 1020));
-        driver.get(" https://the-internet.herokuapp.com");
-        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-        allLinks.get(10).click();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement linkToCheckboxes = driver.findElement(By.cssSelector(".fa.fa-home"));
+        linkToCheckboxes.click();
+        WebElement linkToCheckboxes1 = driver.findElement(By.cssSelector("#site-menu .category-1"));
+        linkToCheckboxes1.click();
+        WebElement element = driver.findElement(By.cssSelector(".link[href=\"https://litecart.stqa.ru/en/rubber-ducks-c-1/blue-duck-p-4\"]"));
+        Assert.assertTrue(element.isDisplayed());
         driver.quit();
     }
+
 }
