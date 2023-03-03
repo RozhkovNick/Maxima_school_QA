e92f223b61d9bca4fc2da7ba514bd62b99284251
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-03-03 19:10:36 +0300

    Update TestMain.java

--- TestNg/src/test/java/TestMain.java
+++ TestNg/src/test/java/TestMain.java
@@ -2,20 +2,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestMain extends BeforeAndAfter {
+    NextPage nextPage = new NextPage(driver);
 
-    @Test(priority = 1, description = "Test button Home")
-    public void homeClick() {
-
-        NextPage nextPage = new NextPage(driver);
+    @Test(description = "Тест на кнопку Home")
+    public void buttonHome() {
+        nextPage.buttonRubberDucksClick();
+        Assert.assertTrue(nextPage.successMessageIsVisible(), "Text is false");
+        
         nextPage.buttonHomeClick();
         Assert.assertEquals(driver.getCurrentUrl(), "https://litecart.stqa.ru/en/");
     }
 
-    @Test(priority = 2, description = "Test button Rubber Ducks")
-    public void rubberDucks() {
-        NextPage nextPage = new NextPage(driver);
+    @Test(description = "Тест на кнопку Rubber Ducks")
+    public void buttonRubberDuck() {
         nextPage.buttonRubberDucksClick();
-
         Assert.assertTrue(nextPage.successMessageIsVisible(), "Text is false");
     }
 }
