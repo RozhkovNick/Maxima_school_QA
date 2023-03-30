369be297479de2f02f81468549af52853f04f360
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-03-30 18:38:00 +0300

    Update HomePage.java

--- FluensProject/src/Page/HomePage.java
+++ FluensProject/src/Page/HomePage.java
@@ -5,14 +5,17 @@ public class HomePage {
     }
 
     public HomePage openMenu() {
+        System.out.println("Открыть меню");
         return this;
     }
 
     public HomePage selectShop(ShopMainPage searchShopDescription) {
+        System.out.println("Выбрать магазин");
         return this;
     }
 
     public ShopMainPage enterShop() {
+        System.out.println("Кликнуть на магазин");
         return new ShopMainPage();
     }
 }
