70c7099266450d8a9ce5f8094f0873180b448234
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-08-21 20:00:48 +0300

    Add files via upload

---
+++ RestCalculatorTest/src/main/java/calculator/service/RestCalculatorService.java
@@ -0,0 +1,35 @@
+package calculator.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+public class RestCalculatorService {
+
+
+    public BigDecimal add(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    public BigDecimal division(BigDecimal a, BigDecimal b) {
+        if (b.equals(BigDecimal.ZERO)) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a.divide(b, 5, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal pow(BigDecimal a, int pow) {
+        return a.pow(pow);
+    }
+}
