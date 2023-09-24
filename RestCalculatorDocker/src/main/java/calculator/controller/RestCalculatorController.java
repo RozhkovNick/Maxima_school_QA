4454e379e1ef0d38a9e7865293130323d21367ee
Рожков Николай Николаевич <115650700+RozhkovNick@users.noreply.github.com>
2023-09-24 19:51:05 +0300

    Add files via upload

---
+++ RestCalculatorDocker/src/main/java/calculator/controller/RestCalculatorController.java
@@ -0,0 +1,47 @@
+package calculator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import calculator.service.RestCalculatorService;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("calculator/v1")
+
+public class    RestCalculatorController {
+    RestCalculatorService calculatorService;
+
+    public RestCalculatorController(RestCalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/add")
+    public ResponseEntity<BigDecimal> add(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+        return ResponseEntity.ok(calculatorService.add(a, b));
+    }
+
+
+    @GetMapping("/subtract")
+    public BigDecimal subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+        return calculatorService.subtract(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public BigDecimal multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+        return calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/division")
+    public BigDecimal division(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+        return calculatorService.division(a, b);
+    }
+
+    @GetMapping("/pow")
+    public BigDecimal pow(@RequestParam BigDecimal a, @RequestParam int pow) {
+        return calculatorService.pow(a,pow);
+    }
+}
